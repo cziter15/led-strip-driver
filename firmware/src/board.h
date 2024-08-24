62b62d31a8a23a79656f88a309e1405f0ff5c3b0
Krzysztof Strehlau <chris.streh@gmail.com>
2024-08-24 14:27:54 +0200

    Update pins

--- firmware/src/board.h
+++ firmware/src/board.h
@@ -10,6 +10,8 @@
 #pragma once
 
 /* Pin number of the LED used as status idicator. */
-#define STATUS_LED_PIN 4
+#define STATUS_LED_PIN 13
 /* Pin number of the LED used as error indicator. */
-#define ERROR_LED_PIN 5
+#define ERROR_LED_PIN 12
+/* Pin number for the data signal driving the strip. */
+#define STRIP_DATA_PIN 5
